13375e7af134abd731dee3151491092b6d1bd591
Nobu <86172592+bunon0@users.noreply.github.com>
2023-07-30 15:49:56 +0900

    ✨ 課題008FizzBuzz

---
+++ kadai_008/kadai_008.py
@@ -0,0 +1,12 @@
+# 【課題】条件分岐のif文で処理を切り替えよう
+
+var = 3
+
+if var % 3 == 0 and var % 5 == 0:
+    print("FizzBuzz")
+elif var % 5 == 0:
+    print("Buzz")
+elif var % 3 == 0:
+    print("Fizz")
+else:
+    print(var)
